ac89fe06f2f7224f141b93341e05664c9001136d
rayan <rhrayan2228@gmail.com>
2021-11-02 11:33:52 +0600

    fixed api iusse

--- script.js
+++ script.js
@@ -3,20 +3,22 @@ const currencyOneElm = document.querySelector('#currency-one');
 const currencyTwoElm = document.querySelector('#currency-two');
 const amountOneElm = document.querySelector('#amount-one');
 const amountTwoElm = document.querySelector('#amount-two');
+const convertElm = document.querySelector('#convert');
 const rateElm = document.querySelector('#rate');
 const swapElm = document.querySelector('#swap');
 // Event Listeners
 const allActions = () => {
-  currencyOneElm.addEventListener('change', validation);
-  currencyTwoElm.addEventListener('change', validation);
-  amountOneElm.addEventListener('input', validation);
+  // currencyOneElm.addEventListener('change', validation);
+  // currencyTwoElm.addEventListener('change', validation);
+  // amountOneElm.addEventListener('input', validation);
   swapElm.addEventListener('click', swapCurrency)
+  convertElm.addEventListener('click', validation)
 }
 
 // validation 
 const validation = () => {
   if (amountOneElm.value <= 0) {
-    rateElm.innerText = "not accept negative number"
+    rateElm.innerText = "not accepted negative number or zero"
     rateElm.style.color = "#2FDD92"
   } else {
     rateElm.style.color = ""
@@ -27,7 +29,7 @@ const validation = () => {
 async function calculate() {
   currencyOneVal = currencyOneElm.value
   currencyTwoVal = currencyTwoElm.value
-  await fetch(`https://free.currconv.com/api/v7/convert?q=${currencyOneVal}_${currencyTwoVal}&compact=ultra&apiKey=4b3e0f853d05719f6d4e`).then(response => response.json()).then(result => {
+  await fetch(`https://v1.nocodeapi.com/rayan2228/cx/LPHapxmkyFYalsnp/rates/convert?amount=100&from=${currencyOneVal}&to=${currencyTwoVal}`).then(response => response.json()).then(result => {
     showValue(result)
   })
 }
@@ -35,8 +37,8 @@ async function calculate() {
 const showValue = (data) => {
   currencyOneVal = currencyOneElm.value
   currencyTwoVal = currencyTwoElm.value
-  const rate = Object.values(data)
-  rateElm.innerText = `1 ${currencyOneVal} = ${rate} ${currencyTwoVal}`
+  const rate = data.info.rate
+  rateElm.innerText = `1 ${currencyOneVal} = ${rate.toFixed(2)} ${currencyTwoVal}`
   amountTwoElm.innerText = (amountOneElm.value * rate).toFixed(2)
 }
 //  swap currency
@@ -48,8 +50,6 @@ const swapCurrency = () => {
 };
 
 // call function
-validation()
-calculate()
 allActions()
 
 // https://free.currconv.com/api/v7/currencies?apiKey=4b3e0f853d05719f6d4e
